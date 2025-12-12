9a27d157adb0cfaadbf1677038bd897474111ff5
Sagar Kapoor <sagarbadal70@gmail.com>
2025-12-12 12:16:21 +0530

    updated resposive desing

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded',() => {
     document.querySelectorAll('.accordion-header').forEach(header => {
         header.addEventListener('click', () => {
             const item = header.parentElement;
